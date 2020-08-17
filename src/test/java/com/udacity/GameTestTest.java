9058715d58fc1fba0543c99fcf26135a45bc8bdc
Daniel.Laga <daniellaga84@gmail.com>
2020-08-17 20:46:59 +0200

    Game test done and Game.Java also

---
+++ src/test/java/com/udacity/GameTestTest.java
@@ -0,0 +1,82 @@
+package com.udacity;
+
+import junit.framework.TestCase;
+
+public class GameTestTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testTestSetUp() {
+    }
+
+    public void testHorizontal_x_win_case1() {
+    }
+
+    public void testHorizontal_x_win_case2() {
+    }
+
+    public void testHorizontal_x_win_case3() {
+    }
+
+    public void testHorizontal_o_win_case1() {
+    }
+
+    public void testHorizontal_o_win_case2() {
+    }
+
+    public void testHorizontal_o_win_case3() {
+    }
+
+    public void testVertical_x_win_case1() {
+    }
+
+    public void testVertical_x_win_case2() {
+    }
+
+    public void testVertical_x_win_case3() {
+    }
+
+    public void testVertical_o_win_case1() {
+    }
+
+    public void testVertical_o_win_case2() {
+    }
+
+    public void testVertical_o_win_case3() {
+    }
+
+    public void testDiagonal_x_win_case1() {
+    }
+
+    public void testDiagonal_x_win_case2() {
+    }
+
+    public void testDiagonal_o_win_case1() {
+    }
+
+    public void testDiagonal_o_win_case2() {
+    }
+
+    public void testTie_game_case1() {
+    }
+
+    public void testTie_game_case2() {
+    }
+
+    public void testTie_game_case3() {
+    }
+
+    public void testNo_winner_case1() {
+    }
+
+    public void testNo_winner_case2() {
+    }
+
+    public void testNo_winner_case3() {
+    }
+
+    public void testNo_winner_case4() {
+    }
+}
